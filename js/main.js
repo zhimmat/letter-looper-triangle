75f1624a17db131bf13f56004492c22bcda99f2d
zhimmat <zeyad.himmat@live.com>
2016-02-26 17:32:48 -0500

    added space

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var looperLetter = prompt('Give me a letter!');
 var counter;
 var i;
+
 for (counter = 0; counter < 10; counter++) {
   for (i = 0; i < counter; i++) {
     document.write(looperLetter);
