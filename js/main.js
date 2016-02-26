4c8b07fa434290cdcfdbcf6cb49f0737c7ab2a37
zhimmat <zeyad.himmat@live.com>
2016-02-26 17:30:47 -0500

    added more js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 var looperLetter = prompt('Give me a letter!');
 var counter;
 var i;
+for (counter = 0; counter < 10; counter++) {
+  for (i = 0; i < counter; i++) {
+    document.write(looperLetter);
+  }
+  document.write('<br>');
+}
